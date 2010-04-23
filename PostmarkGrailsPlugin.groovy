24c145e65a3c3ab8bfc00f2416cc3d6edd7eff6e
Mike Wille <mike.wille@flowz.com>
2010-04-23 13:04:01 -0400

    Initial add of postmark plugin

---
+++ PostmarkGrailsPlugin.groovy
@@ -0,0 +1,85 @@
+class PostmarkGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.0.RC1 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [rest:0.3]
+	def loadAfter = ['mail']
+	def observe = ['controllers','services']
+
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def author = "Mike Wille"
+    def authorEmail = "mike.wille@flowz.com"
+    def title = "Provides integration with Postmark mail service"
+    def description = '''\\
+This plugin provides an API with almost exact parity to the mail plugin.  Installing this plugin
+replaces the sendMail injected method with it's own.  Doing so allows use of Postmark without
+changing any code in your app or in the plugins your app depends on.
+
+It does not support attachments as Postmark doesn't provide that capability.  If attachments
+are used, it will be logged as an error and continue.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/postmark"
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before 
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+		configureSendMail(application, applicationContext)
+    }
+
+    def onChange = { event ->
+		configureSendMail(event.application, event.ctx)
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+	def configureSendMail(application, applicationContext) {
+		log = LogFactory.getLog(PostmarkGrailsPlugin.class);
+		
+		def manager = org.codehaus.groovy.grails.plugins.PluginManagerHolder.pluginManager
+		if(manager?.hasGrailsPlugin("mail")) {
+			log.error "Replacing mail plugin's [sendMail] with postmark plugin's [sendMail]"
+		} else {
+			log.error "Couldn't get ahold of plugin manager."
+		}
+		
+		//adding sendMail to controllers
+	    application.controllerClasses*.metaClass*.sendMail = {Closure callable ->
+			applicationContext.postmarkService?.sendMail(callable)
+		}
+
+		//adding sendMail to all services, besides the mailService of the mail plugin and postmarkService of this plugin
+		application.serviceClasses.each {
+			if(it.metaClass?.getTheClass()?.name != applicationContext.mailService?.metaClass?.getTheClass()?.name && 
+				it.metaClass?.getTheClass()?.name != applicationContext.postmarkService?.metaClass?.getTheClass()?.name) {
+					
+				log.error("Adding sendMail to sevice: ${it}")
+				it.metaClass*.sendMail = {Closure callable ->
+					applicationContext.postmarkService?.sendMail(callable)
+				}
+			}
+		}
+	}
+
+}
---
+++ test/unit/org/grails/mail/PostmarkServiceServiceTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.mail
+
+import grails.test.*
+
+class PostmarkServiceServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
